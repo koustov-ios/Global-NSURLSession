5909fc01368bc1f76f2fe19373df9713a0b2fd03
koustov-ios <kbasu71190@yahoo.com>
2016-03-09 15:43:35 +0530

    Revert "One NSLog added."
    This reverts commit bc814b08a19d424b971906e084e58f5dca038ee6.

--- Global NSURLSession/ViewController.m
+++ Global NSURLSession/ViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    NSLog(@"You are in global nsurlsession class...");
 }
 
 - (void)didReceiveMemoryWarning {
